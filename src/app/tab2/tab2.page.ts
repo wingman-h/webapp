1bfbf794b3f9298c32eb06741f1a25400021f358
te19tsukinoki <te19tsukinoki@g.kumamoto-nct.ac.jp>
2021-05-04 22:56:32 +0900

    karenda- and todo gaiken

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  schedule1: string = "cba";
+  schedule2: string = "cba";
+  schedule3: string = "cba";
+  schedule4: string = "cba";
+  schedule5: string = "cba";
+
+  public form = [
+    { val: ""+this.schedule1, isChecked: false },
+    { val: ""+this.schedule2, isChecked: false },
+    { val: ""+this.schedule3, isChecked: false },
+    { val: ""+this.schedule4, isChecked: false },
+    { val: ""+this.schedule5, isChecked: false },
+  ];
+
   constructor() {}
 
 }
