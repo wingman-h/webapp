55c5af2cba96296b5afbb12c16937020804561ea
te19tsukinoki <te19tsukinoki@g.kumamoto-nct.ac.jp>
2021-05-06 14:33:53 +0900

    daimei

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { GlobalService } from '../global.service';
 
 
@@ -8,30 +8,54 @@ import { GlobalService } from '../global.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
 
-  constructor(
-    public gs: GlobalService,
-    private router: Router,
-  ) {}
+   constructor(
+     public gs: GlobalService,
+     private router: Router,
+   ) { }
 
-  chatMessage: string;
-  repeater: string;
-  Message: string;
-  interval: any;
+   chatMessage: any = {};
+   repeater: string;
+   Message: string;
+   interval: any;
+   postObj: string;
+   postObj1: string;
+   massage: string[] = new Array();
+   commentList: any[] = [];
 
-  sendChatMessage = () => {
-    this.repeater = this.Message;
-  }
 
-  printchat(){
-    this.chatMessage = this.repeater;
-  }
+   ngOnInit(){
+     this.interval = setInterval(() => {
+       this.postcomment1()
+     }, 1000);
+   }
 
-  ngOnInit(){
-    this.interval = setInterval(() => {
-      this.printchat()
-    }, 10000);
-  }
 
+   sendChatMessage = () => {
+     this.postObj['message'] = this.Message;
+     this.postObj['server_id'] = localStorage.send_server_id;
+     this.postObj['user_id'] = localStorage.send_user_id;
+     const body = this.postObj;
+
+     this.gs.http('http://140.227.58.187/tubasa/message_receive.php', body).subscribe(
+     )
+   }
+
+   postcomment1 = () => {
+     this.postObj1['server_id'] = localStorage.send_server_id;
+     const body1 = this.postObj1;
+
+     this.gs.http('http://140.227.58.187/tubasa/message_send.php', body1).subscribe(
+       res => {
+         this.chatMessage = res;
+         this.commentList = [];
+         for (let i = 0, j = this.chatMessage['count']; i <= j; i++ ){
+           this.commentList.push({
+             text: ""+ this.chatMessage["Message"]["messages" + String(i)]["message"], 
+          });
+         }
+       }
+     )
+   }
 }
